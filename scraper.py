f8084db67f9c625f1885c8b86dc71cdf2e1999b1
Kooben209 <43027402+Kooben209@users.noreply.github.com>
2018-09-25 16:59:24 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,23 +10,23 @@ browser = Browser('phantomjs', load_images=False)
 
 browser.visit('https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E984&radius=5.0&sortType=18&includeSSTC=true&keywords=probate%2Cexecutor')
 time.sleep(10)
-if browser.is_element_present_by_xpath('//*[@id="l-searchResults"]/div[29]'):
+#if browser.is_element_present_by_xpath('//*[@id="l-searchResults"]/div[29]'):
 	
-	html = browser.html
-	soup = BeautifulSoup(html, 'html.parser')
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
 
-	searchResults = soup.find("div", {"id" : "l-searchResults"})
-	matches = 0
-	if searchResults is not None:
-		adverts = searchResults.findAll("div", {"id" : lambda L: L and L.startswith('property-')})
-		for advert in adverts:
-			if advert.find("div", {"class" : "propertyCard-keywordTag matched"}) is not None:
-				matches += 1
-		print("Found "+str(matches)+" Matches from "+str(len(adverts))+" Items")
-	else:
-		print('No Search Results\n')
+searchResults = soup.find("div", {"id" : "l-searchResults"})
+matches = 0
+if searchResults is not None:
+	adverts = searchResults.findAll("div", {"id" : lambda L: L and L.startswith('property-')})
+	for advert in adverts:
+		if advert.find("div", {"class" : "propertyCard-keywordTag matched"}) is not None:
+			matches += 1
+	print("Found "+str(matches)+" Matches from "+str(len(adverts))+" Items")
 else:
-	print('Page Element Not Found\n')
+	print('No Search Results\n')
+#else:
+#	print('Page Element Not Found\n')
 browser.quit()
 sys.exit(0)
 
