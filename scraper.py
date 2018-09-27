8dcf2c7ed06ae2f7043871ae93d75bda443a96b0
Kooben209 <43027402+Kooben209@users.noreply.github.com>
2018-09-27 10:17:20 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,9 +12,9 @@ chrome_options.add_argument("disable-infobars")
 chrome_options.add_argument("--disable-extensions")
 
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedriver')
-driver.get("https://www.google.com")
-lucky_button = driver.find_element_by_css_selector("[name=btnI]")
-lucky_button.click()
+driver.get("https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E984&radius=5.0&sortType=18&includeSSTC=true&keywords=probate%2Cexecutor")
+#lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+#lucky_button.click()
 
 print(driver.page_source)
 
