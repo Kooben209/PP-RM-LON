e6b0ccaa0bb89ce4e0175e6070ecd950fea7453e
Kooben209 <43027402+Kooben209@users.noreply.github.com>
2018-09-27 10:03:42 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,35 +1,16 @@
-from   bs4 import BeautifulSoup
-from splinter import Browser
-import sys
-import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
-
-#browser = Browser('firefox', headless=True)
-browser = Browser('chrome', headless=True)
-#browser = Browser('phantomjs', load_images=False)
-
-browser.visit('https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E984&radius=5.0&sortType=18&includeSSTC=true&keywords=probate%2Cexecutor')
-time.sleep(10)
-#if browser.is_element_present_by_xpath('//*[@id="l-searchResults"]/div[29]'):
-	
-html = browser.html
-print(html)
-soup = BeautifulSoup(html, 'html.parser')
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 
 
 
-searchResults = soup.find("div", {"id" : "l-searchResults"})
-matches = 0
-if searchResults is not None:
-	adverts = searchResults.findAll("div", {"id" : lambda L: L and L.startswith('property-')})
-	for advert in adverts:
-		if advert.find("div", {"class" : "propertyCard-keywordTag matched"}) is not None:
-			matches += 1
-	print("Found "+str(matches)+" Matches from "+str(len(adverts))+" Items")
-else:
-	print('No Search Results\n')
-#else:
-#	print('Page Element Not Found\n')
-browser.quit()
-sys.exit(0)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("https://www.google.com")
+lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+lucky_button.click()
 
+print(driver.page_source)
