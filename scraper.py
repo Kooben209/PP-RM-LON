577d4dcd89d9d4c935e5195e3ed3fe419749f1ec
Kooben209 <43027402+Kooben209@users.noreply.github.com>
2018-09-27 10:11:04 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,11 +6,16 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument("--disable-extensions")
 
-
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedriver')
 driver.get("https://www.google.com")
-lucky_button = driver.find_element_by_css_selector("[name=btnI]")
-lucky_button.click()
+#lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+#lucky_button.click()
 
 print(driver.page_source)
+
+driver.quit()
