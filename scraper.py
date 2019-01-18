adf1faf0939790a3bb773e980f9f619a57401958
Kooben209 <Kooben209@Kooben209.com>
2019-01-18 06:50:31 +0000

    Revert "Revert "Update scraper.py""
    This reverts commit c67e6cc9a1c1b306dc0578af9c9ff65df8510bf3.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException, WebDriverException
 import os
 from   bs4 import BeautifulSoup
 import sys
@@ -13,6 +14,8 @@ from decimal import Decimal
 from getSoldPrices import getAllSoldPrices
 import requests
 
+import setEnvs
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
@@ -23,6 +26,7 @@ chrome_options.add_argument("disable-infobars")
 chrome_options.add_argument("--disable-extensions")
 
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedriver')
+#driver = webdriver.Chrome(chrome_options=chrome_options)
 
 def parseAskingPrice(aPrice):
 	try:
@@ -58,8 +62,11 @@ for k, v in filtered_dict.items():
 	try:
 		driver.get(checkURL)
 		numOfPages = int(driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[3]/div/div/div/div[2]/span[3]').text)
-	except:
+	except ValueError:
 		numOfPages = 0	
+	except WebDriverException as e:
+		print("WebDriverException Exception: {0} Message: {1}".format("e message", str(e)))		
+		continue
 
 	print("NumberOfPages:"+str(numOfPages))
 
@@ -70,79 +77,85 @@ for k, v in filtered_dict.items():
 		numNormFeat=0
 		numFeat=0
 		
-		html = driver.page_source
-		soup = BeautifulSoup(html, 'html.parser')
-		
-		searchResults = soup.find("div", {"id" : "l-searchResults"})
-		matches = 0
-		if searchResults is not None:		
-			adverts = searchResults.findAll("div", {"id" : lambda L: L and L.startswith('property-')})
-			numResults = len(adverts)
-			numPreFeat = len(searchResults.findAll("div", {"class" : "propertyCard propertyCard--premium propertyCard--featured"}))
-			numNormFeat = len(searchResults.findAll("div", {"class" : "propertyCard propertyCard--featured"}))
-			numFeat = numPreFeat+numNormFeat
+		try:
+			html = driver.page_source
+			soup = BeautifulSoup(html, 'html.parser')
 			
-			for advert in adverts:
-				reduced=False
-				if advert.find("div", {"class" : "propertyCard-keywordTag matched"}) is not None:
-					advertMatch = {}
-					agent = advert.find("span", {"class" : "propertyCard-branchSummary-branchName"}).text
-					if any(x in agent.lower() for x in excludeAgents):
-						continue;
+			searchResults = soup.find("div", {"id" : "l-searchResults"})
+			matches = 0
+			if searchResults is not None:	
+				adverts = searchResults.findAll("div", {"id" : lambda L: L and L.startswith('property-')})
+				numResults = len(adverts)
+				numPreFeat = len(searchResults.findAll("div", {"class" : "propertyCard propertyCard--premium propertyCard--featured"}))
+				numNormFeat = len(searchResults.findAll("div", {"class" : "propertyCard propertyCard--featured"}))
+				numFeat = numPreFeat+numNormFeat
+				
+				for advert in adverts:
+					reduced=False
+					if advert.find("div", {"class" : "propertyCard-keywordTag matched"}) is not None:
+						advertMatch = {}
+						agent = advert.find("span", {"class" : "propertyCard-branchSummary-branchName"}).text
+						if any(x in agent.lower() for x in excludeAgents):
+							continue
 
-					location = k.replace("MORPH_URL_","").replace("_"," ").title()
-					propLink="https://www.rightmove.co.uk"+advert.find("a", {"class" : "propertyCard-link"}).get('href')
-					propId=re.findall('\d+',propLink)
-					title = advert.find("h2", {"class" : "propertyCard-title"}).text
-					address = advert.find("address", {"class" : "propertyCard-address"}).find("span").text
-					link = advert.find("a", {"class" : "propertyCard-link"})
-					price = parseAskingPrice(advert.find("div", {"class" : "propertyCard-priceValue"}).text)
-					displayPrice = advert.find("div", {"class" : "propertyCard-priceValue"}).text
-					image1 = advert.find("img", {"alt" : "Property Image 1"}).get('src')
-					addedOrReduced = advert.find("span", {"class" : "propertyCard-branchSummary-addedOrReduced"}).text
-					if addedOrReduced != None and addedOrReduced != "":
-						if "Reduced" in addedOrReduced:
-							reduced=True
-						if "yesterday" in addedOrReduced:
-							addedOrReduced=datetime.now().date()- timedelta(days=1)
-						elif "today" in addedOrReduced:	
-							addedOrReduced=datetime.now().date()
+						location = k.replace("MORPH_URL_","").replace("_"," ").title()
+						propLink="https://www.rightmove.co.uk"+advert.find("a", {"class" : "propertyCard-link"}).get('href')
+						propId=re.findall('\d+',propLink)
+						title = advert.find("h2", {"class" : "propertyCard-title"}).text
+						address = advert.find("address", {"class" : "propertyCard-address"}).find("span").text
+						link = advert.find("a", {"class" : "propertyCard-link"})
+						price = parseAskingPrice(advert.find("div", {"class" : "propertyCard-priceValue"}).text)
+						displayPrice = advert.find("div", {"class" : "propertyCard-priceValue"}).text
+						image1 = advert.find("img", {"alt" : "Property Image 1"}).get('src')
+						addedOrReduced = advert.find("span", {"class" : "propertyCard-branchSummary-addedOrReduced"}).text
+						if addedOrReduced != None and addedOrReduced != "":
+							if "Reduced" in addedOrReduced:
+								reduced=True
+							if "yesterday" in addedOrReduced:
+								addedOrReduced=datetime.now().date()- timedelta(days=1)
+							elif "today" in addedOrReduced:	
+								addedOrReduced=datetime.now().date()
+							else:
+								dateMatch = re.search(r'\d{2}/\d{2}/\d{4}', addedOrReduced)
+								addedOrReduced = datetime.strptime(dateMatch.group(), '%d/%m/%Y').date()
 						else:
-							dateMatch = re.search(r'\d{2}/\d{2}/\d{4}', addedOrReduced)
-							addedOrReduced = datetime.strptime(dateMatch.group(), '%d/%m/%Y').date()
-					else:
-						addedOrReduced = datetime.now().date()
-					advertMatch['propId'] = propId[0]
-					advertMatch['link'] = propLink
-					advertMatch['title'] = title
-					advertMatch['address'] = address
-					advertMatch['price'] = price
-					advertMatch['displayPrice'] = displayPrice
-					advertMatch['image1'] = image1
-					advertMatch['pubDate'] = datetime.now()
-					advertMatch['addedOrReduced'] = addedOrReduced
-					advertMatch['reduced'] = reduced
-					advertMatch['location'] = location
+							addedOrReduced = datetime.now().date()
+						advertMatch['propId'] = propId[0]
+						advertMatch['link'] = propLink
+						advertMatch['title'] = title
+						advertMatch['address'] = address
+						advertMatch['price'] = price
+						advertMatch['displayPrice'] = displayPrice
+						advertMatch['image1'] = image1
+						advertMatch['pubDate'] = datetime.now()
+						advertMatch['addedOrReduced'] = addedOrReduced
+						advertMatch['reduced'] = reduced
+						advertMatch['location'] = location
 
-					#scraperwiki.sqlite.save(['propId'],advertMatch)
-					
-					saveToStore(advertMatch)
-					
-					matches += 1
-			print("Found "+str(matches)+" Matches from "+str(numResults)+" Items of which "+str(numFeat)+" are Featured")
-			if matches == 0 or (numResults-numFeat-2)>matches:
-				break		
-		else:
-			print('No Search Results\n')
-		
-		if numOfPages > 1:
-			if page == 0: 
-				close_cookie = driver.find_element_by_css_selector('#cookiePolicy > div > button')
-				close_cookie.click()
-			time.sleep(sleepTime)
-			next_page = driver.find_element_by_css_selector('.pagination-direction--next')
-			next_page.click()
-			time.sleep(sleepTime)
+						#scraperwiki.sqlite.save(['propId'],advertMatch)
+						
+						saveToStore(advertMatch)
+						
+						matches += 1
+				print("Found "+str(matches)+" Matches from "+str(numResults)+" Items of which "+str(numFeat)+" are Featured")
+				if matches == 0 or (numResults-numFeat-2)>matches:
+					break		
+			else:
+				print('No Search Results\n')
+			
+			if numOfPages > 1:
+				if page == 0: 
+					close_cookie = driver.find_element_by_css_selector('#cookiePolicy > div > button')
+					close_cookie.click()
+				time.sleep(sleepTime)
+				next_page = driver.find_element_by_css_selector('.pagination-direction--next')
+				next_page.click()
+				time.sleep(sleepTime)
+				
+		except WebDriverException as e:
+			print("WebDriverException Exception: {0} Message: {1}".format("e message", str(e)))		
+		finally:
+			break
 		page +=1 
 	time.sleep(sleepTime)
 driver.quit()
